ed5071afcc7794884abe865c8cae66807096a93b
Eric Schramm <schramm@careevolution.com>
2018-05-11 18:21:08 -0500

    bugfix - thanks Travis

--- MiniMetroGnomeTests/MiniMetroGnomeTests.swift
+++ MiniMetroGnomeTests/MiniMetroGnomeTests.swift
@@ -30,7 +30,6 @@ class MiniMetroGnomeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        XCTAssertNotNil(mainViewController.bpmLabel)
         XCTAssertNotNil(mainViewController.slider)
         XCTAssertNotNil(mainViewController.toggleButton)
         XCTAssertNotNil(mainViewController.clickSound)
